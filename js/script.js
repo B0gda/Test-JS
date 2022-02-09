542cb2559599d3206106a46c06c41dcdc162ce07
Bogdan <bogda077077@gmail.com>
2022-02-09 23:21:41 +0400

    add 35 lesson

--- js/script.js
+++ js/script.js
@@ -868,5 +868,18 @@ for(let node of document.body.childNodes){
 //События на мобильном устройстве
 
 window.addEventListener('DOMContentLoaded', () => {
-    
+    const box = document.querySelector('.box');
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        console.log('start');
+        console.log(e.touches);
+    });
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+        console.log(e.targetTouches[0].pageX);
+    });
+    box.addEventListener('touchend', (e) => {
+        e.preventDefault();
+        console.log('end');
+    });
 });

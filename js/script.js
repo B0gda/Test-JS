2e14e5eda517eb34d15dac51bfb9597183615b55
Bogdan <bogda077077@gmail.com>
2022-03-28 22:16:16 +0400

    refactor script.js

--- js/script.js
+++ js/script.js
@@ -953,5 +953,7 @@ btn = document.querySelector('button');
 // const width = box.scrollWidth;
 // const height = box.scrollHeight;
 btn.addEventListener('click', () => {
-    box.style.height = box.scrollHeight + 'px';
+    // box.style.height = box.scrollHeight + 'px';
+    console.log(box.scrollTop);
 });
+const style = window.getComputedStyle(box);

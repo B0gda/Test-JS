4168abcb6062b7c101ed748a057c6aac230b1d08
Bogdan <bogda077077@gmail.com>
2022-04-23 20:47:30 +0400

    refactor index.html add MenuCard class

--- js/script.js
+++ js/script.js
@@ -143,9 +143,76 @@ window.addEventListener('DOMContentLoaded', () => {
         if (window.pageYOffset + document.documentElement.clientHeight
             >= document.documentElement.scrollHeight) { //если не работает то document.documentElement.scrollHeight - 1 напиши
             openModal();
-            window.removeEventListener('scroll',showModalByScroll);
+            window.removeEventListener('scroll', showModalByScroll);
         }
     }
-    window.addEventListener('scroll',showModalByScroll); //{once:true} не поможет
+    window.addEventListener('scroll', showModalByScroll); //{once:true} не поможет
+
+
+
+    //Классы для карточек
+    class MenuCard {
+        constructor(src, alt, title, description, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.description = description;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.trasfer = 27;//примитивный  курс валют
+            this.changeToUAH();//можно так сразу вызвать метод и сразу изменить курс
+        }
+        changeToUAH() {
+            this.price = this.price * this.trasfer;
+        }
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+          <div class="menu__item">
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.description}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+              <div class="menu__item-cost">Цена:</div>
+              <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+      </div>
+          `;
+            this.parent.append(element);
+        }
+    }
+    //это похоже
+    // const div = new MenuCard();
+    // div.render();
+    //но
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render(); //когда нужно только 1 раз использовать
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        23,
+        '.menu .container'
+    ).render(); //когда нужно только 1 раз использовать
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        13,
+        '.menu .container'
+    ).render(); //когда нужно только 1 раз использовать
 
 });
+
+
+

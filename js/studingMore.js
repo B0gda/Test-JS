b9f7b2d3eacda9ed1e02c380059dee3d856cc092
Bogdan <bogda077077@gmail.com>
2022-04-25 21:47:39 +0400

    add studingMore.js file

---
+++ js/studingMore.js
@@ -0,0 +1,11 @@
+//переменные
+const a = 5000;
+const b = 4000;
+console.log('Ширина кузова автомобиля: ' + a + ', длина: ' + b);
+//важен стиль и смысл заложенный в названия переменных
+//snake_case
+//UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase 
+
+
